1b398a760166f39006f96a438bbb7a3e55831d06
alfonsosargiottoITEC <a.sargiotto@itecriocuarto.org.ar>
2020-11-13 00:08:18 -0300

    firsts migrations

---
+++ database/migrations/2020_11_13_030146_create_recipes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable(false);
+            $table->string('description')->nullable(false);
+            $table->foreignId('classification')->default(1);
+            $table->foreignId('user_id')->nullable(false)->onDelete('cascade')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
